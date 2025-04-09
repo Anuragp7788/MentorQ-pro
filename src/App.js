790e1bb722976b7ca47ad17fc64c17a2d59f08b1
Anuragp7788 <119566430+Anuragp7788@users.noreply.github.com>
2025-04-09 02:43:28 +0000

    Updated App.js with full single-page app and dark mode theme

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './index.css';
 import Home from './pages/Home';
 import Plans from './pages/Plans';
 import TestPortal from './pages/TestPortal';
 import Contact from './pages/Contact';
 import Login from './pages/Login';
-import Resources from './pages/Resources';
 import Signup from './pages/Signup';
+import Resources from './pages/Resources';
 import About from './pages/About';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <Router>
-      <div className="app">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/plans" element={<Plans />} />
-          <Route path="/test-portal" element={<TestPortal />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/resources" element={<Resources />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/plans" element={<Plans />} />
+        <Route path="/test-portal" element={<TestPortal />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/resources" element={<Resources />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </Router>
   );
 };
